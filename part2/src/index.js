776451110e63bf8f05dda85d468d2752f4ad34be
Denis <denisgao@buffalo.edu>
2020-03-31 10:45:13 -0400

    2B completed

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,59 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import Course from './Course'
 
-const App = () => {
-  const course = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+const Numberform = ({persons,setPersons}) => {
+
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+
+  
+  const submitNumber = (event) => {
+    event.preventDefault();
+    //Filter for same names
+    setPersons(persons.concat({name:newName,number:newNumber}))
+    setNewName("");
+    setNewNumber("");
+  }
+
+  const checkName = (event) => {
+    let found = persons.filter((person)=>event.target.value == person.name)
+    console.log("FOUND:",found)
+    if(found.length>0){
+      alert(`${event.target.value} already exists in our system`)
+    }else{
+      setNewName(event.target.value);
     }
-  ]
+  }
+
   return (
-    <div>
-      {course.map((ele,i)=> <Course key={i} course={ele}></Course>)}
-    </div>
+    <form onSubmit={submitNumber}>
+      <div>
+        name: <input onChange={checkName} value={newName}/>
+      </div>
+      <div>
+        Number: <input onChange={(event)=>setNewNumber(event.target.value)} value={newNumber}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
   )
 }
 
+const Numbers = (props) => {
+
+  return (
+    <ul>
+    {props.people.map((ele)=><li>{ele.name} {ele.number}</li>)}
+    </ul>
+  )
+}
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <hr/>
+      <Numberform persons={persons} setPersons={setPersons}/>
+      <h2>Numbers</h2>
+      <Numbers people={persons}/>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <App />,
