fba9453624513c85f57b45a034a79d2a1ae46d2b
Denis <denisgao@buffalo.edu>
2020-04-22 16:48:41 -0400

    Part4A completed

---
+++ Part4/utils/list_helper.js
@@ -0,0 +1,44 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((a,b) => {
+    return a + b.likes
+  },0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((a,b) => {
+    console.log(a, b)
+    return a.likes>b.likes?a:b
+  },[])
+}
+
+const mostBlogs = (blogs) => {
+
+}
+
+const mostLikes = (blogs) => {
+  var topblog = blogs.reduce((a,b) => {
+    return a.likes>b.likes?a:b
+  },[])
+  var maxLikes = 0
+  blogs.forEach(ele => {
+    if(ele.author === topblog.author){
+      maxLikes+=ele.likes
+    }
+  })
+  return {
+    author:topblog.author,
+    likes:maxLikes
+  }
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
